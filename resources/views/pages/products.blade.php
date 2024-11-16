eaab82279f1b55a9af41235ff3cae5596e0add17
Le Binh Minh <novuslbm2003@gmail.com>
2024-11-16 16:42:52 +0700

    Update: Thêm phân trang cho trang sản phẩm và các sản phẩm được chọn từ danh mục.

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -122,46 +122,50 @@
            
         </div>
         @endforeach
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script>
-             function notifyLogin() {
-                alert("Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng.");
-            }
-            function viewProduct(productId) {
-                $.ajax({
-                    url: '/product/' + productId,  
-                    method: 'GET',
-                    success: function(data) {
-                        $('#productName').text(data.name);
-                        $('#productDetails').text(data.details);
-                        $('#productBrand').text(data.brand);
-                        $('#productPrice').text(data.price.toLocaleString('vi-VN') + ' VNĐ');
-                        $('#productPriceBuy').text(data.price_buy);
-                        $('#productCategory').text(data.category);
-                        
-                        $('#productOrigin').text(data.origin);
-                        $('#productRel').text(data.rel);
-
-                        $('#productConcentration').text(data.concentration);
-                        $('#productFragrance').text(data.fragranceGroup);
-                        $('#productStyle').text(data.style);
-
-                        
-                        $('#productImage1').attr('src', '{{ asset("storage/manual/product/") }}/' + data.images[0]);
-                        $('#productImage2').attr('src', '{{ asset("storage/manual/product/") }}/' + data.images[1]);
-                        $('#productImage3').attr('src', '{{ asset("storage/manual/product/") }}/' + data.images[2]);
-                        $('#productModal').modal('show');
-                    },
-                    error: function() {
-                        alert("Không thể tải thông tin sản phẩm.");
-                    }
-                });
-            }
-            $(document).ready(function() {
-            $('#closeModalButton').click(function() {
-                $('#productModal').modal('hide'); // Ẩn modal
-            });
-        });
-        </script>
+       
     </div>
+    <div class="pagination-container">
+        {{ $products->links('vendor.pagination.bootstrap-4') }}
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+         function notifyLogin() {
+            alert("Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng.");
+        }
+        function viewProduct(productId) {
+            $.ajax({
+                url: '/product/' + productId,  
+                method: 'GET',
+                success: function(data) {
+                    $('#productName').text(data.name);
+                    $('#productDetails').text(data.details);
+                    $('#productBrand').text(data.brand);
+                    $('#productPrice').text(data.price.toLocaleString('vi-VN') + ' VNĐ');
+                    $('#productPriceBuy').text(data.price_buy);
+                    $('#productCategory').text(data.category);
+                    
+                    $('#productOrigin').text(data.origin);
+                    $('#productRel').text(data.rel);
+
+                    $('#productConcentration').text(data.concentration);
+                    $('#productFragrance').text(data.fragranceGroup);
+                    $('#productStyle').text(data.style);
+
+                    
+                    $('#productImage1').attr('src', '{{ asset("storage/manual/product/") }}/' + data.images[0]);
+                    $('#productImage2').attr('src', '{{ asset("storage/manual/product/") }}/' + data.images[1]);
+                    $('#productImage3').attr('src', '{{ asset("storage/manual/product/") }}/' + data.images[2]);
+                    $('#productModal').modal('show');
+                },
+                error: function() {
+                    alert("Không thể tải thông tin sản phẩm.");
+                }
+            });
+        }
+        $(document).ready(function() {
+        $('#closeModalButton').click(function() {
+            $('#productModal').modal('hide'); // Ẩn modal
+        });
+    });
+    </script>
 @endsection
